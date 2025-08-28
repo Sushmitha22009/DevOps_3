c173976fab66335347a8cc1b4ae0089cc8a368c4
Sushmitha22009 <studentaids009@gmail.com>
2025-08-28 15:55:22 +0530

    Add files via upload

--- PATTERN.py
+++ PATTERN.py
@@ -1,10 +1,1 @@
-rows = int(input("Enter the number of rows: "))
-
-for i in range(1, rows + 1):
-    # Printing spaces
-    print(" " * (rows - i), end="")
-    # Printing stars
-    print("*" * (2 * i - 1))
-
-
-
+print("hi")
