eecdf069e1ae659873bdf06dd7170c123a48f530
Sushmitha22009 <studentaids009@gmail.com>
2025-08-28 15:25:26 +0530

    Update PATTERN.py

--- PATTERN.py
+++ PATTERN.py
@@ -1,2 +1,10 @@
-print("Hello this is File1")
+rows = int(input("Enter the number of rows: "))
+
+for i in range(1, rows + 1):
+    # Printing spaces
+    print(" " * (rows - i), end="")
+    # Printing stars
+    print("*" * (2 * i - 1))
+
+
 
